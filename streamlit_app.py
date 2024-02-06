683d820cbd64bdbae86582175336c78c45b436e6
Saurav00216 <159110100+Saurav00216@users.noreply.github.com>
2024-02-06 12:30:54 +0530

    Update streamlit_app.py
    corrected Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner');
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('Breakfast Favorites)
 streamlit.text('Omega 3 & Blueberry Oatmeal');
 streamlit.text('Kale, Spinach & Rocket Smoothie');
-streamlit.text('Hard-Boiled Free-Range Egg');
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
